7caa5c5008ef42c7033d6f0a1e7fb3fd68c8c209
NouranFayez <nouran_ahmed269@hotmail.com>
2022-07-30 10:51:42 +0200

    js session 2

---
+++ JS/session2/js/script.js
@@ -0,0 +1,161 @@
+
+
+/*
+switch(variableName){
+    case variableValue : code ......
+    case variableValue : code ......
+    case variableValue : code ......
+    default : code ....
+}
+
+*/
+
+// var p = prompt("enter your Job")
+
+// switch (p) {
+//     case "doc":
+//         console.log("doctor")
+//         break
+//     case "eng":
+//         console.log("engineer")
+//         break
+//     case "dev":
+//         console.log("developer")
+//         break
+//     case "ins":
+//         console.log("instructor")
+//         break
+//     default: console.log('invalid job')
+// }
+
+
+// var isGraduated = 3
+
+// switch (isGraduated) {
+//     case "true":
+//     case false:
+//     case 3:
+//         console.log("boolean type = true/false")
+//         break
+       
+//     default: console.log("invalid")
+// }
+
+
+//////////////////////////////////////////////////////
+
+///// 
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+/*
+variable 
+while(condition){
+
+
+
+
+    increment++;
+}
+
+*/
+
+// var i = 1
+
+// while(i<= 10){
+//     console.log(i) // 1
+
+//     i++
+// }
+
+
+//////////////////////////
+
+/* 
+    for(variable ; condition ; increment){
+        code ......
+}
+*/
+
+// var startNum = 1
+
+// for(var i = startNum ; i <= 10 ; i++  ){
+   
+//     if(i == 3) continue;
+//     else if(i == 8) break;
+//     else console.log(i)
+
+// }
+
+
+//////////////////////////////////////////////
+
+
+/*
+function name(){
+    code ....
+}
+
+call()
+*/
+
+// function sumTwoNumbers(){
+//    var nums =  4 + 5
+//     return nums
+
+// }
+
+// var result = sumTwoNumbers()
+
+// console.log(result)
+///////////////////////////////
+
+// var res ="correct"
+
+
+// function getFullName(){
+//     var fName = prompt("enter your First Name")
+//     var LName = prompt("enter your Last Name")
+//     console.log(res)
+//     fullName = fName +" "+LName
+// //    return fName +" "+LName
+// }
+
+// // getFullName() ==> firstNme lastName
+// getFullName()
+
+// console.log(fullName)
+/////////////////////////////////////
+
+function sum(num = "" , num1  = "3", num2 = 0  , num3 = 0 ){
+    // console.log(num3) // undefined
+    for(var i = 0 ; i<=5 ; i++){
+        console.log(i)
+    }
+     return num + num1 + num2 + num3
+
+}
+
+var n = 4
+console.log(sum( n , 5))
+console.log(sum(10 , 20 , 1000))
+console.log(sum())
+// console.log(sum(200, 300))
+// console.log(sum(40 , 50))
+
+
+// var p = prompt("enter your name" , "nouran")
+var p = prompt()
+
+
+
+
