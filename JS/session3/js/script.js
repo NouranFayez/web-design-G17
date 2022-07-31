462f829ceec67b3413196407498f586278e63848
NouranFayez <nouran_ahmed269@hotmail.com>
2022-07-31 20:48:42 +0200

    js session 3

--- JS/session3/js/script.js
+++ JS/session3/js/script.js
@@ -49,12 +49,12 @@ var item2 = nums.find(element => element > 100)
 var index = nums.findIndex(item => item > 16)
 var index2 = nums.findIndex(item => item > 166)
 
-console.log(index2)
+// console.log(index2)
 
 var newArray = nums.filter(items => items > 10)
 var newArray = nums.filter(items => items > 100)
 
-console.log(newArray)
+// console.log(newArray)
 
 // console.log(nums.reverse())
 
@@ -63,7 +63,7 @@ var test = [true , false ]
 
 var anotherArray = nums.concat(arr , test)
 
-console.log(anotherArray)
+// console.log(anotherArray)
 
 var item = nums.find(element => element > 10)
 
@@ -76,3 +76,115 @@ var item = nums.find(element => element > 10)
 // function checkAge(age) {
 //   return age > 18;
 // }
+
+///////////////////////////////////////////////////////
+// var names = ["ahmed" , "ali"]
+
+// names[0] 
+// console.log(names[0].join())
+// var start = "a"
+// console.log(names[0].startsWith(start))
+/////////////////////////////////////////////////////
+
+
+
+
+////////////// object
+
+// var user = [ "tamer" , "fayez"  , 30 , "developer" , true]
+
+var user = {
+    firstName : "tamer" ,
+    lastName : "fayez" ,
+    age : 30,
+    job : "developer",
+    isMarried : true 
+}
+
+user.age = 35 // update 
+user.address = ["nasr city" , "october"] // set item
+
+
+console.log(user)
+console.log(user.job) // get item
+
+console.log(user.address.length)
+
+var user2 = {
+    firstName : "Marwa" ,
+    lastName : "Mohamed" ,
+    age : 38,
+    job : "instructor",
+    isMarried : true 
+}
+
+var user3 = {
+    firstName : "mai" ,
+    lastName : "Mohamed" ,
+    age : 40,
+    job : "instructor",
+    isMarried : true 
+}
+
+
+//// Array of objects
+
+
+var users = [
+    {
+        firstName : "tamer" ,
+        lastName : "fayez" ,
+        age : 30,
+        job : "developer",
+        isMarried : true 
+    } ,
+    {
+        firstName : "Marwa" ,
+        lastName : "Mohamed" ,
+        age : 38,
+        job : "instructor",
+        isMarried : true 
+    } ,
+    {
+        firstName : "mai" ,
+        lastName : "Mohamed" ,
+        age : 40,
+        job : "instructor",
+        isMarried : true 
+    }
+
+]
+
+var userInfo ={
+    firstName : "Omar" ,
+    lastName : "Mohamed" ,
+    age : 40,
+    job : "instructor",
+    isMarried : true 
+}
+users.push({
+    firstName : "nada",
+    lastName : "ahmed" ,
+    age : 40,
+    job : "instructor",
+    isMarried : true 
+ } , userInfo )
+
+ 
+ users[2].job = "engineer"
+ users[4].age = 40
+
+
+ 
+ 
+ console.table(users)
+
+users.forEach((ele , ind , arr)=>{
+    console.log(ele.firstName)
+})
+
+var ind = users.findIndex(item => item.firstName == "Omar")
+console.log(ind)
+
+prompt("1 = add , 2 = edit , 3 = delete")
+
